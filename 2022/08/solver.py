ccf14233e7ec522ad2b8f4d929a3ea7000f6d767
Austin Tesch <alrtesch@gmail.com>
2022-12-07 22:45:02 -0800

    solve for part 2

--- 2022/08/solver.py
+++ 2022/08/solver.py
@@ -9,6 +9,8 @@ class AocSolverDay8:
         self.lines = self.data.split('\n')
         self.trees = {}
 
+        self._build_tree_map()
+
     def read_input(self, file):
         """Read the input file"""
         with open(self.input, 'r', encoding='utf-8') as file:
@@ -41,13 +43,73 @@ class AocSolverDay8:
 
         Consider your map; how many trees are visible from outside the grid?
         """
+        return sum(bool(self.is_visible(x, y)) for x, y in self.trees.items())
 
-        # create a hash map of the trees
+    def solve_p2(self):
+        """
+        Solver for Part 2
+
+        Content with the amount of tree cover available,
+        the Elves just need to know the best spot to build their tree house:
+        they would like to be able to see a lot of trees.
+
+        To measure the viewing distance from a given tree,
+        look up, down, left, and right from that tree;
+        stop if you reach an edge or at the first tree
+        that is the same height or taller than the tree under consideration.
+        (If a tree is right on the edge, at least one of its viewing distances will be zero.)
+
+        A tree's scenic score is found by
+        multiplying together its viewing distance in each of the four directions.
+
+        Consider each tree on your map. What is the highest scenic score possible for any tree?
+        """
+        return max(self.get_tree_scores(x, y) for x, y in self.trees.items())
+
+    def get_tree_scores(self, location, height):
+        """
+        A tree's scenic score is found by
+        multiplying together its viewing distance in each of the four directions
+        trees on the edge will have no viewing distance in that direction
+        """
+        x, y = location
+        scoreup, scoredown, scoreleft, scoreright = 0, 0, 0, 0
+
+        # check the top
+        for y2 in range(y - 1, -1, -1):
+            if self.trees[(x, y2)] >= height:
+                scoreup += 1
+                break
+            scoreup += 1
+
+        # check the bottom
+        for y2 in range(y + 1, len(self.lines)):
+            if self.trees[(x, y2)] >= height:
+                scoredown += 1
+                break
+            scoredown += 1
+
+        # check the left
+        for x2 in range(x - 1, -1, -1):
+            if self.trees[(x2, y)] >= height:
+                scoreleft += 1
+                break
+            scoreleft += 1
+
+        # check the right
+        for x2 in range(x + 1, len(self.lines[0])):
+            if self.trees[(x2, y)] >= height:
+                scoreright += 1
+                break
+            scoreright += 1
+
+        return scoreup * scoredown * scoreleft * scoreright
+
+    def _build_tree_map(self):
+        """build the tree map"""""
         for y, line in enumerate(self.lines):
             for x, char in enumerate(line):
                 self.trees[(x, y)] = int(char)
-
-        return sum(bool(self.is_visible(x, y)) for x, y in self.trees.items())
 
     def is_visible(self, location, height):
         """
@@ -90,7 +152,7 @@ class AocSolverDay8:
 
         return False
 
-    def visualize(self):
+    def visualize_taller_trees(self):
         """
         Visualize the trees, putting brackets around the visible trees
         """
@@ -102,12 +164,18 @@ class AocSolverDay8:
                     print(f" {char} ", end='')
             print()
 
-    def solve_p2(self):
-        """Solver for Part 2"""
-        pass
+    def visualize_tree_scores(self):
+        """
+        Visualize the trees, replacing each tree with its score
+        """
+        for y, line in enumerate(self.lines):
+            for x, char in enumerate(line):
+                print(f"{self.get_tree_scores((x, y), int(char)):3}", end='')
+            print()
 
 if __name__ == '__main__':
     solver = AocSolverDay8('2022/08/input.txt')
     print(f"Solution Part 1: {solver.solve_p1()}")
-    # print(f"Solution Part 2: {solver.solve_p2()}")
-    # solver.visualize()
+    print(f"Solution Part 2: {solver.solve_p2()}")
+    # solver.visualize_taller_trees()
+    # solver.visualize_tree_scores()
