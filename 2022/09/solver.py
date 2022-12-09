56f740d0e28c0297269b00844f5331e58a21b42c
Austin Tesch <alrtesch@gmail.com>
2022-12-08 20:44:13 -0800

    create files for day 9 solver

---
+++ 2022/09/solver.py
@@ -0,0 +1,28 @@
+"""
+No Moudles
+"""
+class AocSolverDay9:
+    """A class for solving the Advent of Code 2022 Day 9 puzzle"""
+    def __init__(self, inputfile):
+        self.input = inputfile
+        self.data = self.read_input(self.input)
+        self.lines = self.data.split('\n')
+
+    def read_input(self, file):
+        """Read the input file"""
+        with open(self.input, 'r', encoding='utf-8') as file:
+            return file.read()
+
+    def solve_p1(self):
+        """Solver for Part 1"""
+        pass
+
+    def solve_p2(self):
+        """Solver for Part 2"""
+        pass
+
+if __name__ == '__main__':
+    solver = AocSolverDay9('2022/08/input.txt')
+    print(f"Solution Part 1: {solver.solve_p1()}")
+    # print(f"Solution Part 2: {solver.solve_p2()}")
+    
