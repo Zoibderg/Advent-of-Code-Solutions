55537ac4839019f71aebb4c71b19a2af864af04a
Austin Tesch <alrtesch@gmail.com>
2022-12-10 20:15:30 -0800

    solve for part 2

--- 2022/10/solver.py
+++ 2022/10/solver.py
@@ -35,50 +35,122 @@ class AocSolverDay10:
         Find the signal strength during the 20th, 60th, 100th, 140th, 180th, and 220th cycles.
         What is the sum of these six signal strengths?
         """
-        register_cycles = self.cycle_counter(self.lines)
+        # create a CPU object
+        cpu = CPU()
 
-        # get the signal strength for the 20th, 60th, 100th, 140th, 180th, and 220th cycles
-        signal_strengths = [self.get_signal_strength(register_cycles, i) for i in range(20, 240, 40)]
+        # perfrom all the operations
+        cpu.preform_operations(self.lines)
 
-        return sum(signal_strengths)
-    def cycle_counter(self, instructions):
-        """
-        Track the cycle number and the value of the X register
-        """
-        registers = {}
-        cycle = 0
-
-        for instruction in instructions:
-            line = instruction.strip().split(' ')
-            # the instruction is in the form of "<command> <value>"
-            command = line[0]
-            if command == 'addx':
-                cycle += 2
-                registers[cycle]= int(line[1])
-            elif command == 'noop':
-                cycle += 1
-            else:
-                raise ValueError(f"Unknown instruction: {instruction}")
-
-        return registers
-
-    def get_signal_strength(self, registers, target_cycle):
-        """
-        Calculate the signal strength
-        """
-        strength = 1
-        for cycle, value in registers.items():
-            if cycle < target_cycle:
-                strength += value
-            else:
-                break
-        return strength * target_cycle
+        # return the signal strength
+        return cpu.signal_strength
 
     def solve_p2(self):
-        """Solver for Part 2"""
-        pass
+        """
+        Solver for Part 2
+        
+        It seems like the X register controls the horizontal position of a sprite.
+        Specifically, the sprite is 3 pixels wide, and the X register sets
+        the horizontal position of the middle of that sprite.
+        In this system, there is no such thing as "vertical position":
+        if the sprite's horizontal position puts its pixels where the CRT is currently
+        drawing, then those pixels will be drawn.
+
+        You count the pixels on the CRT: 40 wide and 6 high.
+        This CRT screen draws the top row of pixels left-to-right,
+        then the row below that, and so on.
+        The left-most pixel in each row is in position 0, and
+        the right-most pixel in each row is in position 39.
+
+        Like the CPU, the CRT is tied closely to the clock circuit: 
+        the CRT draws a single pixel during each cycle. 
+        Representing each pixel of the screen as a #, here are the cycles 
+        during which the first and last pixel in each row are drawn
+
+        So, by carefully timing the CPU instructions and the CRT drawing operations,
+        you should be able to determine whether the sprite is visible the instant
+        each pixel is drawn. If the sprite is positioned such that one of its three
+        pixels is the pixel currently being drawn, the screen produces a lit pixel (#);
+        otherwise, the screen leaves the pixel dark (.).
+        """
+        # create a CRT object
+        crt = CTR()
+
+        # draw the screen
+        crt.draw_screen(self.lines)
+
+        # print the CRT
+        crt.print_crt()
+
+        # return
+        return 'See CRT'
+
+class CPU:
+    """CLASS FOR REPRESENTING THE CPU"""
+    def __init__(self):
+        self.x_register = 1
+        self.signal_strength = 0
+        self.ticks = 0
+
+    def increment_ticks(self):
+        """Increment the ticks and add to the signal strength"""""
+        self.ticks += 1
+        if self.ticks in range(20, 221, 40):
+            self.signal_strength += self.x_register * self.ticks
+
+    def execute_instruction(self, instruction):
+        """Execute the instruction"""
+        line = instruction.strip().split(' ')
+        command = line[0]
+
+        if command == 'addx':
+            self.increment_ticks()
+            self.increment_ticks()
+            self.x_register += int(line[1])
+        elif command == 'noop':
+            self.increment_ticks()
+
+    def preform_operations(self, operations):
+        """Preform the operations"""
+        for operation in operations:
+            self.execute_instruction(operation)
+
+class CTR:
+    """CLASS FOR REPRESENTING THE CRT"""
+    def __init__(self):
+        self.ticks = 0
+        self.x_register = 1
+        self.crt = ['.' for _ in range(240)]
+
+    def draw_pixel(self, pos):
+        """Determines if the pixel is lit or not"""""
+        if self.ticks % 40 in [pos-1, pos, pos+1]:
+            self.crt[self.ticks] = '#'
+        self.ticks += 1
+
+    def execute_instruction(self, instruction):
+        """Execute the instruction"""
+        line = instruction.strip().split(' ')
+        command = line[0]
+
+        if command == 'addx':
+            self.draw_pixel(self.x_register)
+            self.draw_pixel(self.x_register)
+            self.x_register += int(line[1])
+        elif command == 'noop':
+            self.draw_pixel(self.x_register)
+
+    def draw_screen(self, operations):
+        """Draw the screen to the CRT"""
+        for operation in operations:
+            self.execute_instruction(operation)
+
+    def print_crt(self):
+        """Print the CRT"""
+        for row in range(6):
+            print(''.join(self.crt[row*40:row*40+40]))
+
 
 if __name__ == '__main__':
     solver = AocSolverDay10('2022/10/input.txt')
     print(f"Solution Part 1: {solver.solve_p1()}")
-    # print(f"Solution Part 2: {solver.solve_p2()}")
+    print(f"Solution Part 2: {solver.solve_p2()}")
